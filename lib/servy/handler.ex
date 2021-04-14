a3eb65dd0b9bec1a9ffbfd187078ad60098be62b
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-04-13 22:52:32 -0400

    Create basic http request route

---
+++ lib/servy/handler.ex
@@ -0,0 +1,41 @@
+# Think - transforming data with functions
+
+defmodule Servy.Handler do
+    def handle(request) do
+
+        request |> parse |> route |> format_response
+    end
+
+    def parse(request) do
+        # TODO: PArse the request string into a map
+        conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+    end
+
+    def route(conv) do
+        # Create a new map that also has the response body:
+        conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+    end
+
+    def format_response(conv) do
+        # Use values in the map to create an HTTP response string
+        """
+        HTTP/1.1 200 OK
+        Content-Type: text-html
+        Content-Length: 20
+
+        Bears, Lions, Tigers
+        """
+    end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
