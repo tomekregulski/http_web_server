84a44ec755e49d821ca7f7156be139cdf4beebba
Tomek Regulski <tomek.regulski@gmail.com>
2022-02-19 20:51:47 -0500

    Add send/receive for snapshots

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,7 +4,8 @@ defmodule Servy.Handler do
 
   alias Servy.Conv
   alias Servy.BearController
-
+  alias Servy.VideoCam
+  
   @pages_path Path.expand("../../pages", __DIR__)
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
@@ -15,13 +16,29 @@ defmodule Servy.Handler do
     request 
     |> parse
     |> rewrite_path
-    #|> log 
+    |> log 
     |> route 
     |> track
     |> format_response
   end
 
-  def route(%Conv{ method: "GET", path: "/kaboom" } = conv) do
+  def route(%Conv{ method: "GET", path: "/snapshots" } = conv) do
+    parent = self() # the request-handling process
+
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-1")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-2")}) end)
+    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-3")}) end)
+
+    snapshot1 = receive do {:result, filename} -> filename end
+    snapshot2 = receive do {:result, filename} -> filename end
+    snapshot3 = receive do {:result, filename} -> filename end
+
+    snapshots = [snapshot1, snapshot2, snapshot3]
+
+    %{ conv | status: 200, resp_body: inspect snapshots}
+  end
+
+  def route(%Conv{ method: "GET", path: "/kaboom" }) do
     raise "Kaboom!"
   end
 
--- mix.exs
+++ mix.exs
@@ -1,11 +1,11 @@
-defmodule Servy.MixProject do
+defmodule Servy.Mixfile do
   use Mix.Project
 
   def project do
     [
       app: :servy,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -14,7 +14,7 @@ defmodule Servy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :eex]
     ]
   end
 
